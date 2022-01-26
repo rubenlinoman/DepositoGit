31c4212e11e22c2c7f7d7830926e41b9f4139fcc
ruben <ruben@RUBEN-PC>
2022-01-26 20:22:54 +0100

    Segundo commit

--- Deposito/src/cuentas/Main.java
+++ Deposito/src/cuentas/Main.java
@@ -11,13 +11,13 @@ public class Main {
 		try {
 			cuenta1.retirar(2300);
 		} catch (Exception e) {
-			System.out.print("Fallo al retirar");
+			System.out.print("Erro ao retirar");
 		}
 		try {
-			System.out.println("Ingreso en cuenta");
+			System.out.println("Ingreso en conta");
 			cuenta1.ingresar(695);
 		} catch (Exception e) {
-			System.out.print("Fallo al ingresar");
+			System.out.print("Erro ao ingresar");
 		}
 	}
 
@@ -26,7 +26,7 @@ public class Main {
 		double saldoActual;
 		cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
 		saldoActual = cuenta1.estado();
-		System.out.println("El saldo actual es" + saldoActual);
+		System.out.println("O seu saldo actual é" + saldoActual);
 		return cuenta1;
 	}
 }
